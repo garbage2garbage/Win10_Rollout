5c8badab66eb08b484dc5785cf8766c7f96882a9
cv007 <32439630+cv007@users.noreply.github.com>
2018-06-01 19:11:11 -0500

    Add files via upload

--- Win32Apps/install.cmd
+++ Win32Apps/install.cmd
@@ -1,26 +1,36 @@
-@echo off
+ @echo off
 cd /d %~dp0
 color 9f
 rem ===========================================================================
-rem  choose which app(s) wanted
+rem     can pass 'all' to script to automatically do all installs
+rem     else will ask for each, then install only requested apps
 rem ===========================================================================
-set /p zip=    "install 7Zip?         [Y,N] "
-set /p vlc=    "install VLC?          [Y,N] "
-set /p chrome= "install Chrome?       [Y,N] "
-set /p foxit=  "install Foxit Reader? [Y,N] "
-set /p office= "install LibreOffice?  [Y,N] "
+if "%1"=="all" (
+    set zip=y
+    set vlc=y
+    set chrome=y
+    set foxit=y
+    set office=Y
+) else (
+    set /p zip=    "install 7Zip?         [Y,N] "
+    set /p vlc=    "install VLC?          [Y,N] "
+    set /p chrome= "install Chrome?       [Y,N] "
+    set /p foxit=  "install Foxit Reader? [Y,N] "
+    set /p office= "install LibreOffice?  [Y,N] "
+)
 
 rem ===========================================================================
-rem  set app name- can update apps without the need to change this script
+rem     get file names so can update versions easily- assuming the install
+rem     files are named similar enough
 rem ===========================================================================
 for /f %%n in ('dir /b 7z*.exe') do set zipnam=%%n
-for /f %%n in ('dir /b vlc-*.exe') do set vlcnam=%%n
+for /f %%n in ('dir /b vlc*.exe') do set vlcnam=%%n
 for /f %%n in ('dir /b chrome*.exe') do set chromenam=%%n
 for /f %%n in ('dir /b LibreOffice*.msi') do set officenam=%%n
-for /f %%n in ('dir /b FoxitReader*.exe') do set foxitnam=%%n
+for /f %%n in ('dir /b FoxitReader*.msi') do set foxitnam=%%n
 
 rem ===========================================================================
-rem  7Zip
+rem     7Zip
 rem ===========================================================================
 if %zip% == y if exist 7z*.exe (
     <nul set /p nothing=installing 7Zip...
@@ -29,7 +39,9 @@ if %zip% == y if exist 7z*.exe (
 )
 
 rem ===========================================================================
-rem  VLC Media PLayer
+rem     VLC Media PLayer
+rem     can use a vlc settings file from existing user if wanted
+rem     also will delete some extra unneeded links in the start menu
 rem ===========================================================================
 if %vlc% == y if exist vlc-*.exe (
     <nul set /p nothing=installing VLC...
@@ -43,7 +55,8 @@ if %vlc% == y if exist vlc-*.exe (
 )
 
 rem ===========================================================================
-rem  Chrome (master preferences set as needed)
+rem     Chrome
+rem     simple master_preferences file to set home page and startup page
 rem ===========================================================================
 if %chrome% == y if exist Chrome*.exe (
     <nul set /p nothing=installing Chrome...
@@ -53,21 +66,25 @@ if %chrome% == y if exist Chrome*.exe (
 )
 
 rem ===========================================================================
-rem  LibreOffice
+rem     LibreOffice
 rem ===========================================================================
 if %office% == y if exist LibreOffice*.msi (
-    <nul set /p nothing=LibreOffice...
-    msiexec /qb /i %officenam% REGISTER_ALL_MSO_TYPES=1  REGISTER_DOC=1
+    <nul set /p nothing=installing LibreOffice...
+    msiexec /qb /i %officenam% REGISTER_ALL_MSO_TYPES=1 REGISTER_DOC=1
     echo DONE
 )
 
 rem ===========================================================================
-rem  Foxit Reader (run manually, cannot find command line options)
-rem               (choose options carefully- disconnect internet to skip
-rem                having to choose some add-ons they want to push)
+rem     Foxit Reader
+rem     get msi version, then can use command line switches
+rem     user registry settings handled in firstrun.cmd
+rem     delete unneeded extra link in start menu
+rem     Edge will still be set as default pdf viewer- have to manually change
+rem     (Foxit user registry setting is set to not notify if default viewer)
 rem ===========================================================================
-if %foxit% == y if exist FoxitReader*.exe (
-    <nul set /p nothing=Foxit Reader manual install...
-    %foxitnam%
+if %foxit% == y if exist FoxitReader*.msi (
+    <nul set /p nothing=installing Foxit Reader...
+    msiexec /qb /i %foxitnam% ADDLOCAL=FX_PDFVIEWER,FX_SE MAKEDEFAULT="1" VIEW_IN_BROWSER="1" DESKTOP_SHORTCUT="1" STARTMENU_SHORTCUT="1" AUTO_UPDATE="0" CPDF_DISABLE="1" DISABLE_UNINSTALL_SURVEY="1"
+    del "C:\ProgramData\Microsoft\Windows\Start Menu\Programs\Foxit Reader\Activate Plugins.lnk" >NUL
     echo DONE
 )
