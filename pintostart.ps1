855ac7cbddd58eed4ece449e535fa2a963b4a2b7
cv007 <32439630+cv007@users.noreply.github.com>
2018-06-03 17:10:22 -0500

    Add files via upload

--- pintostart.ps1
+++ pintostart.ps1
@@ -1,33 +1,9 @@
-<#
+#=================
+# pintostart.ps1
+#=================
 
-pintostart.ps1
-
-unpin all apps from the start menu
-(with a renamed DefaultLayouts.xml, will only be edge, settings, store)
-pin apps from list provided in this script
-
-taskbar- can unpin (mostly), but not pin
-
-taskbar pinned apps will be done by reg setting in firstrun.reg as
-explorer does not want to unpin for some reason
-
-this will not remove any suggested apps which normally show up for
-a new user when DefaultLayouts.xml is used- there may be a way but
-I do not know how (without brute force in regsitry to all start menu)
-
-#>
-
-# get collection of all app objects
-# (from shell:AppsFolder virtual apps folder)
-$allappobj = (New-Object -Com Shell.Application).NameSpace('shell:::{4234d49b-0245-4df3-b780-3893943456e1}').Items()
-
-# menu strings
-$pin_str = "&Pin to Start"
-$unpin_str = "Un&pin from Start"
-$upintbar_str = "Unpin from tas&kbar"
-
-# apps we want to pin
-$mypinlist = @(
+# apps wanted pinned to start menu- change as wanted
+$apps_topin = @(
     "Microsoft Edge"
     "Calculator"
     "Settings"
@@ -39,15 +15,52 @@ $mypinlist = @(
     "Windows Defender Security Center"
 )
 
-# remove all apps
+<#
+
+1> unpin all apps from the start menu and taskbar
+(with a renamed DefaultLayouts.xml, will only be edge, settings, store in start menu)
+
+2> pin apps to start menu from list provided in this script
+
+taskbar- can unpin, but not pin 
+
+this will not remove any suggested apps which normally show up for
+a new user when DefaultLayouts.xml is used- there may be a way but
+I do not know how (without brute force in registry to all start menu)
+
+#>
+
+# create com object
+$sh = new-object -com Shell.Application
+
+# get collection of all app objects
+$allappobj = $sh.NameSpace('shell:AppsFolder').Items()
+
+# to get a list of app names, can run this command after above command
+#foreach($app in $allappobj){ echo $app.Name }
+
+# menu strings
+$pin_str = "&Pin to Start"
+$unpin_str = "Un&pin from Start"
+$unpintb_str = "Unpin from tas&kbar"
+
+# remove all apps from start menu and taskbar
 foreach($appobj in $allappobj){
-    if($v = $appobj.Verbs() | where Name -eq $unpin_str){ $v.DoIt() } 
+    if($v = $appobj.Verbs() | where Name -eq $unpin_str){ $v.DoIt() }
+    if($v = $appobj.Verbs() | where Name -eq $unpintb_str){ $v.DoIt() }
 }
 
-# pin apps we want
-foreach($nam in $mypinlist){ 
+# pin apps we want to start menu
+foreach($nam in $apps_topin){ 
     if($appobj = $allappobj | where Name -eq $nam){
         if($v = $appobj.Verbs() | where Name -eq $pin_str){ $v.DoIt() } 
     }
 }
+
+# need alternate method for File Explorer taskbar
+# the lnk in the following folder works ok
+$flnk = $sh.Namespace("C:\ProgramData\Microsoft\Windows\Start Menu Places").Items() | ?{ $_.Path -like '*File Explorer*' }
+if($v = $flnk.Verbs() | where Name -eq $unpintb_str){ $v.DoIt() }
+
+
 
