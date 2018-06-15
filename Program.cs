a4ac14ecf2102e0498ab565a030f32709620cf0d
cv007 <32439630+cv007@users.noreply.github.com>
2018-06-15 08:34:52 -0500

    Update Program.cs

--- Program.cs
+++ Program.cs
@@ -358,5 +358,25 @@ W = Windows Store App
 [  W] Alarms & Clock
 [S W] Calculator
 
+
+
+
+public void SetSystemTimeZone(string timeZoneId)
+{
+    var process = Process.Start(new ProcessStartInfo
+    {
+        FileName = "tzutil.exe",
+        Arguments = "/s \"" + timeZoneId + "\"",
+        UseShellExecute = false,
+        CreateNoWindow = true
+    });
+
+    if (process != null)
+    {
+        process.WaitForExit();
+        TimeZoneInfo.ClearCachedData();
+    }
+}
+
      */
 
