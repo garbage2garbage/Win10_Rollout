37e3745f655f76cd4ffcced1d439f1e16de8bc53
cv007 <32439630+cv007@users.noreply.github.com>
2018-06-15 08:23:02 -0500

    Add files via upload

--- Program.cs
+++ Program.cs
@@ -5,12 +5,15 @@ using System.Text;
 using System.Threading.Tasks;
 using System.Runtime.InteropServices;
 using System.IO;
+using System.Management.Automation;
+using System.Management.Automation.Runspaces;
+
 
 namespace ConsoleApp
 {
     class Program
     {
-        enum PinArg { NONE, UNPIN, UNPINTASKBAR, UNPINALL, PIN, LIST, RESTORE };
+        enum PinArg { NONE, UNPIN, UNPINTASKBAR, UNPINALL, PIN, LIST };
         public const string NL = "\r\n";
         public static string this_exe; //name of this script without path, set in main
         public const string version = "1.00";
@@ -26,19 +29,16 @@ namespace ConsoleApp
                 "                   to save a list to a file-" + NL +
                 "                   c:\\>" + this_exe + " -list > saved.txt" + NL +
                 NL +
-                "   -restore        to restore from a list saved file-" + NL +
-                "                   c:\\>" + this_exe + " -restore saved.txt" + NL +
-                NL +
                 "   -pin            appname1 [ appname2 \"app name 3\" ... ]" + NL +
-                "   -unpin          | one or more space separated app name(s)" + NL +
-                "   -unpintaskbar   | app names with spaces need to be quoted" + NL +
-
-                //"   -pin            appname1 [ appname2 \"app name 3\" ... ]" + NL +
-                //"   -unpin          appname1 [ appname2 \"app name 3\" ... ]" + NL +
-                //"   -unpintaskbar   appname1 [ appname2 \"app name 3\" ... ]" + NL +
+                "                     * one or more space separated app name(s)" + NL +
+                "                     * app names with spaces need to be quoted" + NL +
+                "                   filename  (-list format file)" + NL +
+                NL +
+                "   -unpin          appname1 [ appname2 \"app name 3\" ... ]" + NL +
+                "   -unpintaskbar   appname1 [ appname2 \"app name 3\" ... ]" + NL +
                 NL +
                 "   -unpinall       unpin all apps from the start menu and taskbar" + NL +
-                "                   (placeholders/suggested apps will not be removed)" + NL
+                "                     * placeholders/suggested apps will not be removed" + NL
                 );
 
             Environment.Exit(1);
@@ -62,7 +62,7 @@ namespace ConsoleApp
 
             //get command line options
             //check first option- need valid option as first arg
-            if (args.Count() == 0) {
+            if (args.Length == 0) {
                 usage();
             } else {
                 switch (args[0].ToLower()) {
@@ -71,40 +71,24 @@ namespace ConsoleApp
                     case "-unpinall": p = PinArg.UNPINALL; break;
                     case "-unpintaskbar": p = PinArg.UNPINTASKBAR; break;
                     case "-pin": p = PinArg.PIN; break;
-                    case "-restore": p = PinArg.RESTORE; break;
                     //invalid arg
                     default: usage(); break;
                 }
             }
 
+            //if provided a -list formatted file to -pin option, use that
+            if (p == PinArg.PIN && args.Length == 2 && File.Exists(args[1])) {
+                foreach (string line in System.IO.File.ReadAllLines(args[1])) {
+                    if (line.Length > 4 && line.StartsWith("[S")) {
+                        appslist.Add(line.Substring(5).ToLower().Trim());
+                    };
+                }
+            }
             //get command line app name options if option requires it
-            if (p == PinArg.UNPIN || p == PinArg.UNPINTASKBAR || p == PinArg.PIN) {
+            else if (p == PinArg.UNPIN || p == PinArg.UNPINTASKBAR || p == PinArg.PIN) {
                 foreach (string str in args.Skip(1)) {
                     appslist.Add(str.ToLower().Trim());
                 }
-            }
-
-            //if -restore, fill in list from file name in arg[1]
-            //then set p to same as -pin
-            //(ends up effectively same as -pin with command line list)
-            if (p == PinArg.RESTORE) {
-                //check if file name not provided
-                if (args.Count() < 2) { usage(); }
-                //or file not found
-                if (!File.Exists(args[1])) {
-                    Console.WriteLine(args[1] + " -> file not found");
-                    Environment.Exit(1);
-                }
-                //read file lines, add to apps list if [B] or [S] app (same as -list format)
-                //convert all to lowercase
-                //will be same as command line provided list
-                foreach (string line in System.IO.File.ReadAllLines(args[1])) {
-                    if (line.Count() > 4 && line.StartsWith("[B] ") || line.StartsWith("[S] ")) {
-                        appslist.Add(line.Substring(4).ToLower().Trim());
-                    };
-                }
-                //now same as -pin option
-                p = PinArg.PIN;
             }
 
             //convert list to array
@@ -151,10 +135,10 @@ namespace ConsoleApp
                 NL +
                 "  " + this_exe + " : apps list with status" + NL +
                 "===============================================" + NL +
-                "  [ ] = not pinned" + NL +
-                "  [S] = Start menu pinned" + NL +
-                "  [T] = Taskbar pinned" + NL +
-                "  [B] = Both start menu and taskbar pinned" + NL +
+                "  [  ] = not pinned" + NL +
+                "  [S ] = Start menu pinned" + NL +
+                "  [ T] = Taskbar pinned" + NL +
+                "  [ST] = Both start menu and taskbar pinned" + NL +
                 "===============================================" + NL
                 );
             }
@@ -200,14 +184,179 @@ namespace ConsoleApp
 
                 //-list format - [?] app name  ( ? = space, B, S, or T )
                 if (p == PinArg.LIST) {
-                    string s = "[ ] ";                              //not pinned
-                    if (is_tb_pinned && is_pinned) { s = "[B] "; }  //pinned start and taskbar
-                    else if (is_pinned) { s = "[S] "; }             //pinned start
-                    else if (is_tb_pinned) { s = "[T] "; }          //pinned taskbar
+                    string s = "[  ] ";                              //not pinned
+                    if (is_tb_pinned && is_pinned) { s = "[ST] "; }  //pinned start and taskbar
+                    else if (is_pinned) { s = "[S ] "; }             //pinned start
+                    else if (is_tb_pinned) { s = "[ T] "; }          //pinned taskbar
                     Console.WriteLine(s + Nam);
                 }
             }
-            if (p == PinArg.LIST) { Console.WriteLine(""); }
+            if (p == PinArg.LIST) { Console.WriteLine(); }
         }
     }
 }
+
+
+/*
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Principal;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using Windows.ApplicationModel;
+using Windows.Foundation;
+using Windows.Management.Deployment;
+
+namespace ConsoleApp1
+{
+    class RemApp
+    {
+        static void Main(string[] args)
+        {
+
+            //get apps in appsfolder namespace
+            Type t = Type.GetTypeFromProgID("Shell.Application");
+            dynamic shell = Activator.CreateInstance(t);
+            var appobj = shell.NameSpace("shell:AppsFolder");
+            var maxl = 0;
+            foreach (var item in appobj.Items())
+            {
+                if (item.Path.Contains("!") && (item.Name.Trim().Length > maxl)) {
+                    maxl = item.Name.Trim().Length;
+                }
+            }
+            foreach (var item in appobj.Items())
+            {
+                if (item.Path.Contains("!"))
+                {
+                    Console.WriteLine("{0,-" + maxl + "}    {1}", item.Name, item.Path.ToString().Split('_')[0]);
+                }
+                else {
+                    Console.WriteLine(item.Name);
+                }
+            }
+
+
+
+            //Console.WriteLine(WindowsIdentity.GetCurrent().Name);
+
+                PackageManager packageManager = new Windows.Management.Deployment.PackageManager();
+            IEnumerable<Package> packages =
+                packageManager.FindPackagesForUser(WindowsIdentity.GetCurrent().User.Value);
+
+
+            foreach (Package package in packages)
+            {
+                Console.WriteLine(package.Id.Name);
+            }
+
+            if (args.Length == 1) {
+                foreach (Package package in packages)
+                {
+                    if (package.Id.Name == args[0]) {
+                        remove_package(package);
+                    }
+                }
+
+            }
+
+            Console.ReadKey();
+        }
+
+
+        static int remove_package(Windows.ApplicationModel.Package package)
+        {
+            var returnValue = 0;
+            Console.Write(package.Id.Name + "...");
+            PackageManager packageManager = new Windows.Management.Deployment.PackageManager();
+
+            IAsyncOperationWithProgress<DeploymentResult, DeploymentProgress> deploymentOperation =
+                packageManager.RemovePackageAsync(package.Id.FullName);
+            // This event is signaled when the operation completes
+            ManualResetEvent opCompletedEvent = new ManualResetEvent(false);
+
+            // Define the delegate using a statement lambda
+            deploymentOperation.Completed = (depProgress, status) => { opCompletedEvent.Set(); };
+
+            // Wait until the operation completes
+            opCompletedEvent.WaitOne();
+
+            // Check the status of the operation
+            if (deploymentOperation.Status == AsyncStatus.Error)
+            {
+                DeploymentResult deploymentResult = deploymentOperation.GetResults();
+                Console.WriteLine("Error code: {0}", deploymentOperation.ErrorCode);
+                Console.WriteLine("Error text: {0}", deploymentResult.ErrorText);
+                returnValue = 1;
+            }
+            else if (deploymentOperation.Status == AsyncStatus.Canceled)
+            {
+                Console.WriteLine("Removal canceled");
+            }
+            else if (deploymentOperation.Status == AsyncStatus.Completed)
+            {
+                Console.WriteLine("Removal succeeded");
+            }
+            else
+            {
+                returnValue = 1;
+                Console.WriteLine("Removal status unknown");
+            }
+
+            return returnValue;
+        }
+
+    }
+}
+
+
+
+/*
+ 
+     shell.NameSpace("shell:AppsFolder").Items()
+    Path: Microsoft.BingWeather_8wekyb3d8bbwe!App
+    Name: Weather                             
+    
+    
+Windows.ApplicationModel.Package
+    Id.Name: Microsoft.BingWeather
+    Id.FamilyName: Microsoft.BingWeather_8wekyb3d8bbwe
+    
+    
+shell.items() .Path.Split('_')[0] = package.Id.Name 
+
+
+-list
+-restore
+-pin
+-unpin
+-unpintaskbar
+-unpinall
+
+-unwinapp
+
+-wallpaper file | folder (random)
+
+
+
+
+
+S = Start Menu pinned
+T = Taskbar pinned
+W = Windows Store App
+
+[S  ] SageTV 9
+[   ] SageTV 9 Service Control
+[   ] Manual
+[S  ] sPlan 7.0
+[   ] sPlan 7.0 - Viewer
+[S W] Weather
+[  W] Mixed Reality Viewer
+[  W] Voice Recorder
+[  W] Alarms & Clock
+[S W] Calculator
+
+     */
+
