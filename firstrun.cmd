cb8fdf671dbf8a46b4d96752370c1d298d533bc8
cv007 <32439630+cv007@users.noreply.github.com>
2018-07-01 15:47:57 -0500

    Update firstrun.cmd

--- firstrun.cmd
+++ firstrun.cmd
@@ -7,51 +7,22 @@ rem     firstrun.cmd
 rem
 rem     runs when new user created, located in
 rem     %userprofile%\appdata\roaming\microsoft\windows\start menu\programs\startup
-rem     deletes itself when done
-rem
-rem     if script run from somewhere else, no files deleted
-rem     (if want to manually run script on current user)
+rem     deletes itself when done (if run from startup folder)
 rem ===========================================================================
 
 rem ===========================================================================
 rem     set some vars
 rem ===========================================================================
-set firstrun=0
-set logout=0
 set startfolder=%userprofile%\AppData\Roaming\Microsoft\Windows\Start Menu\Programs\Startup
 set usertemp=%userprofile%\appdata\local\temp
 set wallpaperfolder=c:\users\public\pictures
 set appone=%usertemp%\appone.exe
 set appxlist=%usertemp%\appxlist.txt
 set pinstartlist=%usertemp%\pinstartlist.txt
-if "%~dp0" == "%startfolder%\" set firstrun=1
-set silent=^>NUL 2^>^&1
-
-rem ===========================================================================
-rem     give user a choice to run this
-rem ===========================================================================
-echo ==========================================================================
-echo    new Windows user startup script
-echo ==========================================================================
-echo    this script will cleanup the start menu, remove unneeded apps,
-echo    change various settings, set wallpaper, etc, so you can have a
-echo    pleasant Windows experience :)
-echo.
-echo    if you would rather skip this, and use all the default settings, then
-echo    press N when asked to continue
-echo.
-choice /M "   continue? "
-rem Y=1 N=2
-if %errorlevel% == 2 goto :cleanup
-
-
 
 cls
-rem since Y, set to logout automatically when done
-set logout=1
 echo.
-echo  Starting firstrun.cmd, this will not take long
-echo  you will be logged out shortly, so just log back in at logon screen
+echo  Starting firstrun.cmd, this will not take long...
 echo.
 
 rem ===========================================================================
@@ -88,29 +59,14 @@ echo.
 %appone% -weather
 echo.
 
-
-echo.
 color 2f
-echo  will be logging out so some changes can take effect...
-timeout /t 30
-rem ===========================================================================
-rem copy this file to temp IF was firstrun (keep temp files around if needed)
-rem delete this file from startup folder
-rem (goto) trick gets all following commands cached so
-rem logoff command can still run when deleted
-rem ===========================================================================
-:cleanup
-rem if run from startup folder
-if %firstrun% == 1 (
-    copy /y "%~f0" "%usertemp%" %silent%
-    if %logout% == 1 (
-        (goto) 2>nul & del "%~f0" & shutdown /l
-    ) else (
-        (goto) 2>nul & del "%~f0"
-    )
+rem if run from startup folder, delete firstrun.cmd
+if "%~dp0" == "%startfolder%\" (
+    echo DONE (console window will close in 10 seconds)
+    timeout /t 10
+    (goto) 2>nul & del "%~f0"
 )
-rem else was run from somewhere else, do not delete any files, just logout
-if %logout% == 1 shutdown /l
+
 
 
 
